b097f77646993f0590f02f6ba2ffcc995d55180f
fiqryq <fiqrychoerudin48@gmail.com>
2022-06-11 20:23:18 +0700

    feat: add bottomsheet

---
+++ src/components/button/types/index.ts
@@ -0,0 +1,4 @@
+export type IButton = {
+    label: string
+    onClick?: () => void
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      borderRadius: {
+        10: "0.625rem",
+      },
+      maxWidth: {
+        480: "480px",
+      },
+      inset: {
+        "60-px": "60px",
+      },
+      width: {
+        "1/7": "14.2857143%",
+      },
+      zIndex: {
+        1: 1,
+      },
+    },
   },
   plugins: [],
 };
