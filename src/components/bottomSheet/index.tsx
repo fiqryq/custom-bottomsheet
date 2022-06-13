2470dd2ed7c023fda255dc7805a8fba32ccbb8d8
fiqryq <fiqrychoerudin48@gmail.com>
2022-06-13 13:39:33 +0700

    enchanment: update some view

--- src/components/bottomSheet/index.tsx
+++ src/components/bottomSheet/index.tsx
@@ -7,13 +7,17 @@ export const BottomSheet = ({
 }: IBottomSheet): JSX.Element => {
   return (
     <div
-      hidden={!open}
-      className="absolute bottom-0 z-20 w-full rounded-t-lg bg-gray-300 p-5 shadow-sm"
+      className={`fixed bottom-0 ${
+        open ? "z-20 opacity-100 ease-in" : "-z-20 opacity-0 ease-out"
+      } max-w-480 rounded-t-xl bg-white shadow-sm transition duration-150`}
     >
-      <div>
+      <div className="relative border-b-[1px] py-4">
+        <div className="absolute left-3 top-0 flex h-full flex-col items-center justify-center">
+          <div className="h-5 w-5 cursor-pointer bg-slate-500"></div>
+        </div>
         <h1 className="text-center text-xl font-semibold">{title}</h1>
       </div>
-      {children}
+      <div className="p-3">{children}</div>
     </div>
   );
 };
--- src/layout/container/index.tsx
+++ src/layout/container/index.tsx
@@ -2,7 +2,7 @@ import { IContainer } from "./types";
 
 export const Container = ({ children }: IContainer) => {
   return (
-    <main className="relative mx-auto h-screen max-w-480 bg-gray-300/20">
+    <main className="relative mx-auto h-screen max-w-480 bg-gray-500/20">
       {children}
     </main>
   );
